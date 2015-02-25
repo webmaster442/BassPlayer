4769880a18642748445a265cb4a04c46d66d6f22
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2015-02-25 12:51:02 +0100

    Spectrum daemon initial work

---
+++ BassSpectrumDaemon/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Shell;
+using System;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace BassSpectrumDaemon
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application, ISingleInstanceApp
+    {
+        private const string Unique = "BassSpectrumDaemon";
+
+        [STAThread]
+        public static void Main()
+        {
+            if (SingleInstance<App>.InitializeAsFirstInstance(Unique))
+            {
+                var application = new App();
+                application.Run();
+                // Allow single instance code to perform cleanup operations
+                SingleInstance<App>.Cleanup();
+            }
+        }
+
+        public bool SignalExternalCommandLineArgs(IList<string> args)
+        {
+            return true;
+        }
+    }
+}
